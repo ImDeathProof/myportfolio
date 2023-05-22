0ef780692a96eb223a30fff3326d0556e4fd9f67
DeathProof! <71523428+ImDeathProof@users.noreply.github.com>
2023-05-22 14:45:40 -0300

    Proyectos funcionando, casi terminado

---
+++ functions.js
@@ -0,0 +1,10 @@
+function redirection(tipo){
+    if(tipo == 1){
+        window.location.href = "https://github.com/ImDeathProof/GranCerdo"
+    }
+    else if(tipo == 2){
+        window.location.href = "https://github.com/ImDeathProof/BomberMan"
+    }else if(tipo == 3){
+        window.location.href = "https://github.com/ImDeathProof/Prog3-TP2"
+    }
+}
