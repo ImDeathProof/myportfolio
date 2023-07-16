e87cc0ed51f60fa42d9ab50f0166faf2ada7a15d
DeathProof! <71523428+ImDeathProof@users.noreply.github.com>
2023-07-16 13:43:44 -0300

    Agregando Proyecto Almacen de Bebidas
    +Tarjeta del proyecto Almacen de Bebidas
    +Haciendo el fondo un poco mas ancho

--- functions.js
+++ functions.js
@@ -6,5 +6,7 @@ function redirection(tipo){
         window.location.href = "https://github.com/ImDeathProof/BomberMan"
     }else if(tipo == 3){
         window.location.href = "https://github.com/ImDeathProof/Prog3-TP2"
+    }else if(tipo == 4){
+        window.location.href = "https://github.com/ImDeathProof/TPFinal-Grupo18"
     }
 }
